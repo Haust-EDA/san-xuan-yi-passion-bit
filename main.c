599d7a4e5638dd51f35dc9d94487f6c396abf55a
1name <2338313028@qq.com>
2024-11-16 11:56:18 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
-//请在下方输入你的代码：
+include <stdio.h>
+
+void printPositiveTriangle() 
+{
+    for (int i = 1; i <= 9; i++) 
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d * %d = %-2d  ", j, i, i * j);
+        }
+        printf("\n");
+    }
+}
+
+void printInvertedTriangle()
+{
+    for (int i = 9; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d * %d = %-2d  ", j, i, i * j);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    printf("正三角九九乘法表：\n");
+    printPositiveTriangle();
+    printf("倒三角九九乘法表：\n");
+    printInvertedTriangle();
+    return 0;
+}
